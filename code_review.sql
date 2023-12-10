d82100a8f963a8474a099e851b473f545f903e8d
nbanks062523 <nbanks@kingsleytechpartners.com>
2023-12-10 13:52:40 -0500

    answered second question of 'Manipulate values' section

--- code_review.sql
+++ code_review.sql
@@ -18,3 +18,16 @@ SELECT p.id,p.name FROM part_categories as p WHERE name LIKE '%Bricks%';
 -- Then show the names of all he sets where the theme_id matches an id in that subquery
 SELECT name,theme_id FROM sets WHERE theme_id IN (SELECT id FROM themes WHERE name LIKE '%Pirates%' or name like '%Star Wars%');
 
+-- Select the ids of the values in the inventories table that have more than one VERSION
+-- Then select everything from the inventory_parts table where the inventory_id matches an id that subquery
+-- limit the output to 10
+SELECT * FROM inventory_parts WHERE inventory_id IN (SELECT id FROM inventories WHERE version > 1)LIMIT 10;
+
+-- Alias the sets table as 's' and select the year and name of the values in that table that
+-- include the substring 'Batman'
+-- Concatenate three exclamation points to the end of each name and make all he names upper case
+SELECT s.year,CONCAT(UPPER(s.name),'!!!') FROM sets s WHERE s.name LIKE '%Batman%';
+
+-- For every quantity value greater than 1 in the inventory_parts table, double the value of the quantity
+-- Limit the output to 20 rows and order by quantity
+SELECT quantity,quantity * 2 AS double_quantity FROM inventory_parts WHERE quantity >1 ORDER BY quantity DESC LIMIT 20;
