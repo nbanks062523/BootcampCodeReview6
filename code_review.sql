e93aff3f6ecb22575274e234e65aa1c1e5e6b289
nbanks062523 <nbanks@kingsleytechpartners.com>
2023-12-10 12:36:08 -0500

    added question 3

--- code_review.sql
+++ code_review.sql
@@ -8,3 +8,8 @@ SELECT name,part_num from parts WHERE name LIKE '%Hair%' ORDER BY part_num DESC;
 -- From the sets table, select all the sets that included the word 'Showdown'in the name between 1990 and 2015
 select * from sets;
 SELECT * FROM sets WHERE name LIKE '%Showdown%' AND year BETWEEN 1990 AND 2015;
+
+-- Alias the part_categories table as p , show the id and name of the values in that TABLE
+-- where the name includes the word Bricks
+SELECT * FROM part_categories;
+SELECT p.id,p.name FROM part_categories as p WHERE name LIKE '%Bricks%';
